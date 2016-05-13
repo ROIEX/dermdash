626d603d274861dbd28b2daddc46d9fc4fdf8aa7
ROIEX <rfp777@gmail.com>
2016-05-13 17:48:29 +0300

    distance calculation

--- frontend/modules/api/v1/models/GetDoctorList.php
+++ frontend/modules/api/v1/models/GetDoctorList.php
@@ -34,13 +34,69 @@ class GetDoctorList extends Model
     /**
      * @return array
      */
+//    public function getDoctorList()
+//    {
+//        $curl = new Curl();
+//       // $curl->setOption(CURLOPT_CAINFO, Yii::getAlias('@base') . "../../cacert.pem");
+//        $inquiryDoctorList = InquiryDoctorList::findAll([
+//            'inquiry_id' => $this->inquiry_id
+//        ]);
+//
+//        if (empty($inquiryDoctorList)) {
+//            Inquiry::deleteAll(['id' => $this->inquiry_id]);
+//            InquiryBrand::deleteAll(['inquiry_id' => $this->inquiry_id]);
+//            InquiryTreatment::deleteAll(['inquiry_id' => $this->inquiry_id]);
+//            \Yii::$app->response->setStatusCode(409);
+//            return Yii::t('app', 'No doctors are available on your inquiry');
+//        }
+//
+//        $returnData = [];
+//        foreach ($inquiryDoctorList as $list) {
+//            /* @var $list InquiryDoctorList */
+//            $userProfile = UserProfile::findOne(['user_id' => $list->user_id]);
+//            if (!empty($returnData[$userProfile->user_id])) {
+//                $returnData[$userProfile->user_id]['price'] += $list->price;
+//            } else {
+////                $result = $curl->get('https://www.zipcodeapi.com/rest/' . Yii::$app->params['zipCodeServiceApiKey'] .'/distance.json/'. $userProfile->zipcode .'/'. Yii::$app->user->identity->userProfile->zipcode .'/mile');
+////                $distace_obj = json_decode($result);
+////                if (!isset($distace_obj->distance)) {
+////                    $distance = Yii::t('app', 'Sorry, unable to calculate');
+////                } else {
+////                    $distance = round($distace_obj->distance, 2) . ' miles';
+////                }
+//
+//                $returnData[$userProfile->user_id] = [
+//                    'doctor_id'=>$userProfile->user_id,
+//                    'clinic'=> $list->user->doctor->clinic,
+//                    'city' => $list->user->userProfile->city,
+//                    //distance' => $distance ,
+//                    'photo'=>$userProfile->avatar_path ? $userProfile->avatar_base_url . '/' . $userProfile->avatar_path : false,
+//                    'price'=>$list->price,
+//                    'rating'=>[
+//                        'stars'=>$userProfile->rating,
+//                        'reviews'=>$userProfile->reviews
+//                    ],
+//                    'time_after_create'=> time() - $inquiryDoctorList[0]->inquiry->created_at
+//
+//                ];
+//            }
+//
+//        }
+//        return array_values($returnData);
+//    }
+
     public function getDoctorList()
     {
         $curl = new Curl();
-       // $curl->setOption(CURLOPT_CAINFO, Yii::getAlias('@base') . "../../cacert.pem");
-        $inquiryDoctorList = InquiryDoctorList::findAll([
-            'inquiry_id' => $this->inquiry_id
-        ]);
+        // $curl->setOption(CURLOPT_CAINFO, Yii::getAlias('@base') . "../../cacert.pem");
+//        $inquiryDoctorList = InquiryDoctorList::findAll([
+//            'inquiry_id' => $this->inquiry_id
+//        ]);
+
+        $inquiryDoctorList = InquiryDoctorList::find()
+            ->where(['inquiry_id' => $this->inquiry_id])
+            ->with('user')
+            ->all();
 
         if (empty($inquiryDoctorList)) {
             Inquiry::deleteAll(['id' => $this->inquiry_id]);
@@ -52,10 +108,11 @@ class GetDoctorList extends Model
 
         $returnData = [];
         foreach ($inquiryDoctorList as $list) {
+
             /* @var $list InquiryDoctorList */
-            $userProfile = UserProfile::findOne(['user_id' => $list->user_id]);
-            if (!empty($returnData[$userProfile->user_id])) {
-                $returnData[$userProfile->user_id]['price'] += $list->price;
+            //$userProfile = UserProfile::findOne(['user_id' => $list->user_id]);
+            if (!empty($returnData[$list->user->id])) {
+                $returnData[$list->user->id]['price'] += $list->price;
             } else {
 //                $result = $curl->get('https://www.zipcodeapi.com/rest/' . Yii::$app->params['zipCodeServiceApiKey'] .'/distance.json/'. $userProfile->zipcode .'/'. Yii::$app->user->identity->userProfile->zipcode .'/mile');
 //                $distace_obj = json_decode($result);
@@ -64,19 +121,19 @@ class GetDoctorList extends Model
 //                } else {
 //                    $distance = round($distace_obj->distance, 2) . ' miles';
 //                }
-
-                $returnData[$userProfile->user_id] = [
-                    'doctor_id'=>$userProfile->user_id,
+                $profile = $list->user->userProfile;
+                $returnData[$list->user->id] = [
+                    'doctor_id'=>$list->user->id,
                     'clinic'=> $list->user->doctor->clinic,
-                    'city' => $list->user->userProfile->city,
+                    'city' => $profile->city,
                     //distance' => $distance ,
-                    'photo'=>$userProfile->avatar_path ? $userProfile->avatar_base_url . '/' . $userProfile->avatar_path : false,
+                    'photo'=>$profile->avatar_path ? $profile->avatar_base_url . '/' . $profile->avatar_path : false,
                     'price'=>$list->price,
                     'rating'=>[
-                        'stars'=>$userProfile->rating,
-                        'reviews'=>$userProfile->reviews
+                        'stars'=>$profile->rating,
+                        'reviews'=>$profile->reviews
                     ],
-                    'time_after_create'=>time() - $inquiryDoctorList[0]->inquiry->created_at
+                    'time_after_create'=> time() - $inquiryDoctorList[0]->inquiry->created_at
 
                 ];
             }
@@ -84,5 +141,4 @@ class GetDoctorList extends Model
         }
         return array_values($returnData);
     }
-
 }
